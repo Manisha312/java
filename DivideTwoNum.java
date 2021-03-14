6bfa0ca87fb2e7a4045644d46a029b900bed147a
Manisha312 <60319887+Manisha312@users.noreply.github.com>
2021-03-14 21:52:16 +0530

    Add files via upload

---
+++ DivideTwoNum.java
@@ -0,0 +1,9 @@
+public class DivideTwoNum{
+	public static void main(String[] args) {
+		int x=50;
+		int y=3;
+		System.out.println("x="+x+"\ny=" +y);
+		System.out.println("Divide x by y");
+		System.out.println(x+ "/" +y+ "=" +(x/y));
+	}
+}
---
+++ SumTwoNum.java
@@ -0,0 +1,9 @@
+public class SumTwoNum{
+	public static void main(String[] args) {
+		int x=30;
+		int y=55;
+		System.out.println("x="+x+"\ny=" +y);
+		System.out.println(x+ "+" +y);
+		System.out.println(x+y);
+	}
+}
